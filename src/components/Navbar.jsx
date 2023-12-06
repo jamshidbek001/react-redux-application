ef8bf81358bbfc7ad13445a6d8b8397680caf8d6
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2023-12-06 10:52:35 +0100

    Add Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { logo } from '../constants'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div class="container d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom pt-3">
+      <Link to={'/'}>
+        <img src={logo} alt="" />
+      </Link>
+
+      <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+        <Link className='me-3 py-2 link-body-emphasis text-decoration-none' to={'/login'}>
+            Login
+        </Link>
+
+        <Link className='me-3 py-2 link-body-emphasis text-decoration-none' to={'/register'}>
+            Register
+        </Link>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
