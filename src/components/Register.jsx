0f530ea5b2e55c173e3c41614522f162c6c5f3b3
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2023-12-07 11:29:04 +0100

    Redux register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import { icon } from '../constants'
 import {Input} from '../ui'
+import { useDispatch, useSelector } from 'react-redux'
+import {registerUserStart} from '../slice/auth'
+
 
 const Register = () => {
   const [name, setName] = useState('')
   const [email,setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const dispatch = useDispatch()
+  const {isLoading} = useSelector(state => state.auth)
+
+  const registerHandler = e => {
+    e.preventDefault()
+    dispatch(registerUserStart())
+  }
 
   return (
     <div className='text-center mt-5'>
@@ -17,7 +27,7 @@ const Register = () => {
           <Input label={"Username"} state={name} setState={setName} />
           <Input label={"Email"} state={email} setState={setEmail}/>
           <Input label={"Password"} type={'password'} state={password} setState={setPassword} />
-          <button className="btn btn-primary w-100 py-2 mt-4" type="submit">Register</button>
+          <button className="btn btn-primary w-100 py-2 mt-4" disabled={isLoading} onClick={registerHandler}  type="submit">{isLoading ? 'loading..' : 'Register'}</button>
         </form>
       </main>
     </div>
