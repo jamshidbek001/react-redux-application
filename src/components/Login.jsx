dd3e37f38150da2f9a56ac9295a528bfa88ecae7
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2023-12-09 17:37:05 +0100

    Add article detail ui

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,7 +10,7 @@ import { useNavigate } from 'react-router-dom'
 const Login = () => {
   const [email,setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const dispatch = useDispatch(state => state.auth)
+  const dispatch = useDispatch()
   const {isLoading,loggedIn} = useSelector(state => state.auth)
   const navigate = useNavigate()
 
--- src/slice/article.js
+++ src/slice/article.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+
 const initialState = {
     isLoading: false,
     articles: [],
